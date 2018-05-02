57d4beeb0f9fe33850300a0bf35dffb7a24e8196
wanghaoming <whimmy@126.com>
2018-05-02 10:45:02 +0800

    add spring-cloud-netflix-ribbon

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Module | Article
 --|--
 spring-cloud-netflix-zuul | [Spring REST with a Zuul Proxy](http://www.baeldung.com/spring-rest-with-zuul-proxy)
 spring-cloud-netflix-eureka | [Introduction to Spring Cloud Netflix – Eureka](http://www.baeldung.com/spring-cloud-netflix-eureka)
+spring-cloud-netflix-ribbon | [Introduction to Spring Cloud Rest Client with Netflix Ribbon](http://www.baeldung.com/spring-cloud-rest-client-with-netflix-ribbon)
---
+++ spring-cloud-netflix-ribbon/src/test/java/com/ymmihw/spring/cloud/netflix/ribbon/TestConfig.java
@@ -0,0 +1,16 @@
+package com.ymmihw.spring.cloud.netflix.ribbon;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@RestController
+public class TestConfig {
+  @RequestMapping(value = "/locaus")
+  public String locationAUSDetails() {
+    return "Australia";
+  }
+}
