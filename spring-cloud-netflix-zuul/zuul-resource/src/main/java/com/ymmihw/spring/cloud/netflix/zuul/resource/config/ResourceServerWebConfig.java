5a072c415e7bae78bb635019cc1bf2a9a830d8b6
wanghaoming <whimmy@126.com>
2018-05-02 09:40:45 +0800

    add zuul-resource

---
+++ spring-cloud-netflix-zuul/zuul-resource/src/main/java/com/ymmihw/spring/cloud/netflix/zuul/resource/config/ResourceServerWebConfig.java
@@ -0,0 +1,11 @@
+package com.ymmihw.spring.cloud.netflix.zuul.resource.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ResourceServerWebConfig implements WebMvcConfigurer {
+
+}
