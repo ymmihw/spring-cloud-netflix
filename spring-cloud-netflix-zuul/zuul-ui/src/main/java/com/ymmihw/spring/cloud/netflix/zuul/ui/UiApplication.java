c170c9634ce0503f642802bddeb426b1da8d2a1d
wanghaoming <whimmy@126.com>
2018-05-02 10:14:34 +0800

    add zuul-ui

---
+++ spring-cloud-netflix-zuul/zuul-ui/src/main/java/com/ymmihw/spring/cloud/netflix/zuul/ui/UiApplication.java
@@ -0,0 +1,13 @@
+package com.ymmihw.spring.cloud.netflix.zuul.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class UiApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(UiApplication.class, args);
+  }
+}
---
+++ spring-cloud-netflix-zuul/zuul-ui/src/test/java/com/ymmihw/spring/cloud/netflix/zuul/ui/LiveTest.java
@@ -0,0 +1,22 @@
+package com.ymmihw.spring.cloud.netflix.zuul.ui;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class LiveTest {
+
+  @Test
+  public void whenSendRequestToFooResource_thenOK() {
+    final Response response = RestAssured.get("http://localhost:8080/foos/1");
+    assertEquals(200, response.getStatusCode());
+  }
+
+  @Test
+  public void whenSendRequest_thenHeaderAdded() {
+    final Response response = RestAssured.get("http://localhost:8080/foos/1");
+    assertEquals(200, response.getStatusCode());
+    assertEquals("TestSample", response.getHeader("Test"));
+  }
+}
