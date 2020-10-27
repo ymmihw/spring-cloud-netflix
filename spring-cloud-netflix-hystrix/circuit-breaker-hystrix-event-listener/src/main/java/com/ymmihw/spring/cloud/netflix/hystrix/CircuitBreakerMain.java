ff08290a64c0ccc1a7fce6613d45e82c68928b93
wanghaoming <wanghaoming@DESKTOP-G5PV73T>
2020-10-27 09:29:57 +0800

    add circuit-breaker-hystrix-event-listener

---
+++ spring-cloud-netflix-hystrix/circuit-breaker-hystrix-event-listener/src/main/java/com/ymmihw/spring/cloud/netflix/hystrix/CircuitBreakerMain.java
@@ -0,0 +1,23 @@
+package com.ymmihw.spring.cloud.netflix.hystrix;
+
+import com.netflix.hystrix.strategy.HystrixPlugins;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class CircuitBreakerMain {
+  public static void main(String[] args) {
+    HystrixPlugins.getInstance().registerEventNotifier(new MyHystrixNotifier());
+    ConfigurableApplicationContext ctx = SpringApplication.run(CircuitBreakerMain.class, args);
+    MyService myService = ctx.getBean(MyService.class);
+    System.out.println("-- calling doSomething(2) --");
+    myService.doSomething(2);
+    System.out.println("-- calling doSomething(0) --");
+    myService.doSomething(0);
+    System.out.println("-- calling doSomething(5) --");
+    myService.doSomething(5);
+  }
+}
