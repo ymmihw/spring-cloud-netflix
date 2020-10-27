f9cd34315724daf53fe4f6c98a1bad2285d5fe71
wanghaoming <wanghaoming@DESKTOP-G5PV73T>
2020-10-27 09:12:39 +0800

    add circuit-breaker-thread-pool-config

---
+++ spring-cloud-netflix-hystrix/circuit-breaker-thread-pool-config/src/main/java/com/ymmihw/spring/cloud/netflix/hystrix/CircuitBreakerMain.java
@@ -0,0 +1,37 @@
+package com.ymmihw.spring.cloud.netflix.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.ConfigurableApplicationContext;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class CircuitBreakerMain {
+  public static void main(String[] args) throws InterruptedException {
+    ConfigurableApplicationContext ctx = SpringApplication.run(CircuitBreakerMain2.class, args);
+    MyService myService = ctx.getBean(MyService.class);
+
+    System.out.println("Main Thread: " + Thread.currentThread().getName());
+    System.out.println("-- calling doSomething(1) 20 times --");
+    int n = 20;
+    ExecutorService es = Executors.newFixedThreadPool(n);
+    for (int i = 0; i < n; i++) {
+      int finalI = i;
+      es.submit(() -> myService.doSomething(finalI + 1));
+    }
+    es.shutdown();
+    es.awaitTermination(10, TimeUnit.SECONDS);
+
+    TimeUnit.SECONDS.sleep(5);
+    System.out.println("-- after 5 sec --");
+    myService.doSomething(5);
+
+    TimeUnit.SECONDS.sleep(5);
+    System.out.println("-- after 5 sec --");
+    myService.doSomething(2);
+  }
+}
